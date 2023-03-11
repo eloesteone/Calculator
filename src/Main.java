54f2bd498203456f5f12f9031fa8953d401ffea6
eloesteone <barancewiczkacper@gmail.com>
2023-03-11 14:38:52 +0100

    Created new class UI to design a GUI for application and added functionality

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,9 @@
-import java.util.Scanner;
 
-public class Main {
+public class Main extends UI{
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        char operator;
-        System.out.println("Choose operator: +, -, *, /");
-        operator = sc.next().charAt(0);
-
-        System.out.println("Enter first number");
-        double n1 = sc.nextDouble();
-
-        System.out.println("Enter second number");
-        double n2 = sc.nextDouble();
-
-        switch (operator){
-            case '+':
-                System.out.println(n1+n2);
-                break;
-            case '-':
-                System.out.println(n1-n2);
-                break;
-            case '*':
-                System.out.println(n1*n2);
-                break;
-            case '/':
-                System.out.println(n1/n2);
-                break;
-            default:
-                System.out.println("Invalid");
-
-        }
+        new Main();
     }
+
+
 }
