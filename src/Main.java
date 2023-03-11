8767557ce1e84e930141adbb50c701eefd957de1
eloesteone <barancewiczkacper@gmail.com>
2023-03-11 12:13:24 +0100

    Made a simple switch case calculator

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Hello world!");
+        char operator;
+        System.out.println("Choose operator: +, -, *, /");
+        operator = sc.next().charAt(0);
+
+        System.out.println("Enter first number");
+        double n1 = sc.nextDouble();
+
+        System.out.println("Enter second number");
+        double n2 = sc.nextDouble();
+
+        switch (operator){
+            case '+':
+                System.out.println(n1+n2);
+                break;
+            case '-':
+                System.out.println(n1-n2);
+                break;
+            case '*':
+                System.out.println(n1*n2);
+                break;
+            case '/':
+                System.out.println(n1/n2);
+                break;
+            default:
+                System.out.println("Invalid");
+
+        }
     }
 }
